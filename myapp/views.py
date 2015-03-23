1365fe01a35d2dd39cd0febcd1e54c4536f2a867
PJ1959 <jeyasunderam@gmail.com>
2015-03-23 18:10:46 +0530

    first changes

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from myapp import app
-from flask import render_template
+from flask import render_template ,flash,redirect
 from .forms import LoginForm
 
 @app.route('/')
@@ -29,4 +29,9 @@ def index():
 def login():
 
 	form=LoginForm()
-	return render_template('login.html',title='Sign In',form=form)
+
+	if form.validate_on_submit():
+		flash('Login requested for OpenId="%s", remember_me=%s' %(form.openid.data, str(form.remember_me.data)))
+		
+		return redirect('/index')
+	return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
